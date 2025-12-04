2e2a5b151b39dbc6c4bd3bfd6cf477d4c6a002f5
soxd555-a11y <soxd555@gmail.com>
2025-12-04 19:09:32 +0700

    html

--- resources/views/html101.blade.php
+++ resources/views/html101.blade.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<html>
+    <head>
+        <title>ส่วนหัวของ html</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <style>
+            h1{color: red;} 
+        </style>
+        <h1>HTML &nbsp;101</h1>
+        <h1 style="color:blue;color:red;">&lt;HTML 101</h1>
+        <h2>HTML 101</h2>  
+        <h3>HTML <br>101</h3>
+        <hr />
+        <hr>
+        <table border="1" width="100">
+            <tbody>
+                <tr>
+                    <td>ลำดับ</td>
+                    <td>ชื่อ</td>
+        
+                </tr>
+            </tbody>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Nanthawach</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
 </html>
